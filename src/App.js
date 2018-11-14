e060a883c5a3f44d4d374977c731a4982d4a5957
helpful <derek@helpful.com>
2018-11-14 11:18:40 -0500

    End of PWA Experiment tutorial (without the Firebase part)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Route, Link } from 'react-router';
-import logo from './logo.svg';
 import './App.css';
 
 const Page = ({ title }) => (
   <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>{title}</h2>
-    </div>
+    <h2>{title}</h2>
     <p className="App-intro">
       This is the {title} page.
     </p>
@@ -24,9 +20,12 @@ const Page = ({ title }) => (
   </div>
 );
 
-const Home = (props) => (
-  <Page title="Home"/>
-);
+const Home = (props) => {
+  console.log('Rendering the Home page');
+  return (
+    <Page title="Home"/>
+  );
+};
 
 const About = (props) => (
   <Page title="About"/>
